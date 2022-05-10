28bcf7188c2623a14b56799fa870038bea911cf2
W-Elidio <willianelidio19gmail.com>
2022-05-10 04:54:39 -0300

    primeiro js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var list= ["maçã", "pêra", "laranja"];
+list.push("uva")// insere itens a lista.
+list.pop();// retira o ultimo elemento da lista
+console.log(list);
+
+
+//var name="Willian Elidio";
+//var idade= 28;
+//alert(name + " tem " +idade + " anos");
+//var frase="Japão é o melhor time do mundo";
+//console.log(name);
+//console.log(idade);
+//console.log(frase);
+//console.log(frase.replace("Japão", "Brasil"))//trocar algo na frase, nesse caso Japão por Brasil.
