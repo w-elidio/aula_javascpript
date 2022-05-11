c9ea56b2f249393542780f7061ec6a851d00b04f
W-Elidio <willianelidio19gmail.com>
2022-05-11 05:27:31 -0300

    aula js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
-var list= ["maçã", "pêra", "laranja"];
+
+
+/*var d = new Date(); //visualizar data
+alert(d);
+alert(d.getDay());
+alert(d.getHours());*/
+
+
+/*var count;
+for(count = 0; count <= 5; count++){
+    console.log(count)
+};*/
+
+/*var count= 0;
+while (count < 5)// contar enquanto for menor que 5(0,1,2,3,4)
+{ console.log(count);
+count++;}*/
+
+/*var idade = prompt("Qual sua idade?")// o browser exibe uma pergunta para o usuario e o que ele responder será gravado na variavel idade
+if(idade >= 18)
+{ alert("maior de idade"); }
+else
+{ alert("menor de idade");}*/
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas.nome);
+alert(frutas[1].cor);*/
+
+
+/*var list= ["maçã", "pêra", "laranja"];
 list.push("uva")// insere itens a lista.
 list.pop();// retira o ultimo elemento da lista
 console.log(list);
+console.log(list.toString()); //transforma em strings
+console.log(list.join(" - ")); //separa usando (-, /,| entre outros)*/
 
 
 //var name="Willian Elidio";
